5f40c109b7d5a657b8dfa46ca8a5478109bf5b7f
Jacob.Anderson <jacob.anderson@asurion.com>
2024-05-13 10:25:44 -0500

    staged

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addNumbers = (number1, number2) => {
   const sum = number1 + number2;
-  console.log(`Your Number Output is here... ${sum}`);
+  // console.log(`Your Number Output is here... ${sum} lol`);
   return sum;
 };
 module.exports = { addNumbers };
