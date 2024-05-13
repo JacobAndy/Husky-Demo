bf24bed7c8740a47bb7e0602401eee9816177e7b
Jacob.Anderson <jacob.anderson@asurion.com>
2024-05-13 10:29:24 -0500

    test

---
+++ scripts/validate_git_branch.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+PROTECTED_BRANCHES=("master" "main")
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+for protected_branch in "${PROTECTED_BRANCHES[@]}"; do
+    if [[ "${branch}" == $protected_branch ]]; then
+        echo "😵 😱 Uh Oh! 😱 😵 "
+        echo "You cannot commit changes directly to remote branch:${protected_branch}"
+        echo "😵 😱 Uh Oh! 😱 😵 "
+        exit 1
+    fi
+done
+echo "✅  GIT branch validated."
+exit 0
