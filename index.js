1d403131a48d07a71e52341b9c249724231aa9ff
Jacob.Anderson <jacob.anderson@asurion.com>
2024-05-13 20:14:19 -0500

    demo screen shot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addNumbers = (number1, number2) => {
-  const sum = number1 + number2;
+  // const sum = number1 + number2;
   // console.log(`Your Number Output is here... ${sum} lol`);
-  return sum;
+  return number1 + number2;
 };
 module.exports = { addNumbers };
